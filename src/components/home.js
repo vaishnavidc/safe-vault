d4f5f0d083c902c6912f77bf3ddeb4c23ee7e60c
Abdul Sami <sami-abdul@outlook.com>
2018-05-14 13:34:30 +0500

    Added custom set gas functionality

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, Input, Row, Button, Col } from 'react-materialize';
-import {Label} from 'react-bootstrap';
+import { Label } from 'react-bootstrap';
 
 import getWeb3 from '../utils/getWeb3'
 import StorageContract from '../../build/contracts/Storage.json'
@@ -10,6 +10,13 @@ const contract = require('truffle-contract')
 var storageContract
 var deployedInstance
 var mAccounts
+var customGasPrice = 0
+var data = {
+    id1: '',
+    id2: '',
+    address: ''
+}
+var currentState = 'Please select'
 
 class Company extends Component {
     constructor(props) {
@@ -17,7 +24,8 @@ class Company extends Component {
 
         this.state = {
             web3: null,
-            gasCostState: 'Average',
+            gasLimit: 0,
+            gasPrice: 0,
             customStateValue: 0,
             EntryID: '',
             ID1Read: '',
@@ -50,18 +58,29 @@ class Company extends Component {
         })
     }
 
-    addData(data) {
-        let gasEstimate
-        return deployedInstance.addData.estimateGas(data.ID1, data.ID2, data.address)
+    estimateGas() {
+        return deployedInstance.addData.estimateGas(data.id1, data.id2, data.address)
             .then((result) => {
-                gasEstimate = result * 2
-                console.log("Estimated gas to add data: " + gasEstimate)
+                if (currentState === 'Average') {
+                    this.setState({ gasPrice: this.state.web3.eth.gasPrice, gasLimit: result })
+                }
+                else if (currentState === 'Fast') {
+                    this.setState({ gasPrice: this.state.web3.eth.gasPrice * 2, gasLimit: result })
+                }
+                else if (currentState === 'Custom') {
+                    this.setState({ gasPrice: customGasPrice, gasLimit: result })
+                }
             })
+    }
+
+    addData() {
+        let gasEstimate
+        return this.estimateGas()
             .then(() => {
-                return deployedInstance.addData(data.ID1, data.ID2, data.address, {
+                return deployedInstance.addData(data.id1, data.id2, data.address, {
                     from: data.address,
-                    gas: gasEstimate,
-                    gasPrice: this.state.web3.eth.gasPrice
+                    gas: this.state.gasLimit,
+                    gasPrice: this.state.gasPrice
                 })
             })
             .then((result) => {
@@ -80,33 +99,23 @@ class Company extends Component {
 
     writeSubmit(event) {
         event.preventDefault();
-        if (this.refs.ID1.state.value === undefined 
-            || this.refs.ID2.state.value === undefined 
+        if (this.refs.ID1.state.value === undefined
+            || this.refs.ID2.state.value === undefined
             // || this.refs.Address.state.value === undefined 
             // || this.refs.PrivateKey.state.value === undefined 
             || this.state.EntryID === undefined
-        ) 
-            {
+            || this.state.gasCostState === 'Please Select'
+        ) {
             alert("All the fields are required");
         }
         else {
-            let Obj = {
-                ID1: this.refs.ID1.state.value,
-                ID2: this.refs.ID2.state.value,
-                address: mAccounts[0],
-                privateKey: this.refs.PrivateKey.state.value,
-                entryId: this.state.EntryID,
-                gasCost: this.refs.GasCost.state.value,
-                customStateValue: this.refs.customStateValue.state.value
-            }
-            console.log(Obj);
-            this.addData(Obj)
+            this.addData()
         }
     }
 
     readSubmit(event) {
         event.preventDefault();
-        if (this.refs.EntryIDRead.state.value === undefined ) {
+        if (this.refs.EntryIDRead.state.value === undefined) {
             alert("All the fields are required");
         }
         else {
@@ -120,21 +129,41 @@ class Company extends Component {
         }
     }
 
-    dropdownChanged(event){
-        if(event.target.value === 'Average' || event.target.value === 'Fast'){
+    dropdownChanged(event) {
+        event.preventDefault();
+
+        currentState = event.target.value
+        if (event.target.value === 'Average' || event.target.value === 'Fast') {
             document.getElementById('editTitle').value = 0;
             document.getElementById('editTitle').disabled = true;
-
+            data = {
+                id1: this.refs.ID1.state.value,
+                id2: this.refs.ID2.state.value,
+                // address: this.refs.Address.state.value
+                address: mAccounts[0]
+            }
+            this.estimateGas()
         }
-        else if(event.target.value === 'Custom'){
+        else if (event.target.value === 'Custom') {
             document.getElementById('editTitle').disabled = false
+            data = {
+                id1: this.refs.ID1.state.value,
+                id2: this.refs.ID2.state.value,
+                // address: this.refs.Address.state.value
+                address: mAccounts[0]
+            }
+            this.estimateGas()
         }
     }
 
-    customStateValue(event){
+    onCustomGasPriceChanged(event) {
+        customGasPrice = event.target.value
+    }
+
+    customStateValue(event) {
         console.log(this, event.target.value)
         this.setState({
-            customStateValue : event.target.value
+            customStateValue: event.target.value
         })
     }
 
@@ -145,50 +174,51 @@ class Company extends Component {
                     <Tab title="Write" active>
                         <form onSubmit={this.writeSubmit.bind(this)}>
                             <br />
-                            <Row style={{marginBottom : 0}}>
-                            <div > Data: </div>
+                            <Row style={{ marginBottom: 0 }}>
+                                <div > Data: </div>
                                 <Input s={6} type='text' ref='ID1' name='ID1' label="ID 1" />
                                 <Input s={6} type='text' ref='ID2' label="ID 2" name='ID2' />
                             </Row>
-                            <Row style={{marginBottom : 0}}>
-                            <div>Credentials:</div>
-                            <Input s={6} type='text' ref='Address' name='Address' label="Address" />
-                            <Input s={6} type='text' ref='PrivateKey' label="Private Key" name='PrivateKey' />
+                            <Row style={{ marginBottom: 0 }}>
+                                <div>Credentials:</div>
+                                <Input s={6} type='text' ref='Address' name='Address' label="Address" />
+                                <Input s={6} type='text' ref='PrivateKey' label="Private Key" name='PrivateKey' />
                             </Row>
-                            <Row style={{marginBottom : 0}}>
-                                <div>Transaction Gas Cost:</div>
+                            <Row style={{ marginBottom: 0 }}>
+                                <div>Transaction Options:</div>
                                 <div >
-                                <Input s={3} type='select' ref='GasCost' label='GasCost : 10' onChange = {this.dropdownChanged.bind(this)}>
-                                <option value='Fast'>Fast</option>
-                                <option value='Average'>Average</option>
-                                <option value='Custom' >Custom</option>
-                                </Input>
-                                
-                                <Input s={3} type='text' id={'editTitle'} defaultValue = '0' ref = 'customStateValue' name='customStateValue' label="Enter Here" disabled />
-                                
+                                    <Input s={3} type='select' ref='GasCost' onChange={this.dropdownChanged.bind(this)}>
+                                        <option value='Please Select'>Please Select</option>
+                                        <option value='Fast'>Fast</option>
+                                        <option value='Average'>Average</option>
+                                        <option value='Custom' >Custom</option>
+                                    </Input>
+
+                                    <Input s={3} type='number' id={'editTitle'} defaultValue='' onChange={this.onCustomGasPriceChanged.bind(this)} ref='customStateValue' name='customStateValue' label="Enter gas price" disabled />
+                                    <Label s={3} style={{ color: 'blue' }}>Cost: {this.state.gasPrice * this.state.gasLimit}</Label>
                                 </div>
                             </Row >
                             <Row>
-                            <div > Entry ID: </div>
-                            <Label style = {{color : 'blue'}}>{this.state.EntryID}</Label>
+                                <div > Entry ID: </div>
+                                <Label style={{ color: 'blue' }}>{this.state.EntryID}</Label>
                             </Row>
-                            <Button className="btn waves-effect waves-light" type="submit" name="action" title='submit' style={{ display: 'block', margin : 0}}>Submit</Button>
+                            <Button className="btn waves-effect waves-light" type="submit" name="action" title='submit' style={{ display: 'block', margin: 0 }}>Submit</Button>
                         </form>
                     </Tab>
                     {/* Read Tab */}
                     <Tab title="Read">
-                    <form onSubmit={this.readSubmit.bind(this)}>
+                        <form onSubmit={this.readSubmit.bind(this)}>
                             <br />
                             <Row>
-                            <div > Data: </div>
-                            <Col s={1}>ID1:</Col><Col s={5}>{this.state.ID1Read}</Col>
-                            <Col s={1}>ID2:</Col><Col s={5}>{this.state.ID2Read}</Col>
+                                <div > Data: </div>
+                                <Col s={1}>ID1:</Col><Col s={5}>{this.state.ID1Read}</Col>
+                                <Col s={1}>ID2:</Col><Col s={5}>{this.state.ID2Read}</Col>
                             </Row>
                             <Row s={12}>
-                            <div > Entry ID: </div>
+                                <div > Entry ID: </div>
                                 <Input s={6} type='text' ref='EntryIDRead' name='EntryID' label="Type ID here" />
                             </Row>
-                            <Button className="btn waves-effect waves-light" type="submit" name="action" title='submit' style={{ display: 'block', margin : 0}}>Submit</Button>
+                            <Button className="btn waves-effect waves-light" type="submit" name="action" title='submit' style={{ display: 'block', margin: 0 }}>Submit</Button>
                         </form>
                     </Tab>
                 </Tabs>

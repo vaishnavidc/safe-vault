feb94de62fcf79b9682fd4b791a9367b03e8b5bf
Abdul Sami <sami-abdul@outlook.com>
2018-05-20 10:47:04 +0500

    Changed the name of app

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ class CustomNavbar extends Component {
         return (
             <Navbar style = {{backgroundColor : '#2BBBAD'}}>
               <Navbar.Brand>
-                <a href="#" style= {{fontSize : 18}}>BlockSaved</a>
+                <a href="#" style= {{fontSize : 18}}>BlockSave</a>
               </Navbar.Brand>
           </Navbar>
         )

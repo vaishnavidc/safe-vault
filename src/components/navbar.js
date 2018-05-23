a97870e702cafb18876a307a71eaa060dc612b08
maaz28 <maazjawaid28@gmail.com>
2018-05-23 20:18:25 +0500

    navbar updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ class CustomNavbar extends Component {
         return (
             <nav>
             <div className="nav-wrapper" style={{backgroundColor: '#26A69A'}}>
-              <img src={logo} alt='logo'/>
+              <img src={logo} alt='logo' style={{width: "170px"}}/>
               {/* <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="sass.html">Sass</a></li>
                 <li><a href="badges.html">Components</a></li>

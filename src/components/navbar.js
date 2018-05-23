b7ef6f516eafaa9af10fda61d03680671020bf2c
maaz28 <maazjawaid28@gmail.com>
2018-05-23 20:13:30 +0500

    Navbar Updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap';
+import logo from '../images/logo.png';
 
 class CustomNavbar extends Component {
 
     render() {
         return (
-            <Navbar style = {{backgroundColor : '#2BBBAD'}}>
-              <Navbar.Brand>
-                <a href="#" style= {{fontSize : 20}}>BlockSave</a>
-              </Navbar.Brand>
-          </Navbar>
+            <nav>
+            <div className="nav-wrapper" style={{backgroundColor: '#26A69A'}}>
+              <img src={logo} alt='logo'/>
+              {/* <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+              </ul> */}
+            </div>
+          </nav>
         )
     }
 }

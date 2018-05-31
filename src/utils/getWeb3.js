346a0d9330fdb7111ae03b18b3d2a7a939170ad0
Abdul Sami <sami-abdul@outlook.com>
2018-05-31 18:10:32 +0500

    Updated UI

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -18,16 +18,16 @@ let getWeb3 = new Promise(function(resolve, reject) {
     } else {
       console.log("Metamask not installed")
       alert("Metamask not installed on your browser. Please refresh the page after installing.")
-      // var provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545')
-      // web3 = new Web3(provider)
+      var provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545')
+      web3 = new Web3(provider)
 
-      // results = {
-      //   web3: web3
-      // }
+      results = {
+        web3: web3
+      }
 
-      // console.log('No web3 instance injected, using Local web3.');
+      console.log('No web3 instance injected, using Local web3.');
 
-      // resolve(results)
+      resolve(results)
     }
   })
 })

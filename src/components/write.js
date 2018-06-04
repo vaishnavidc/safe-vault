52b379904255f763b2f05cf4bfc746509ea1778a
Abdul Sami <sami-abdul@outlook.com>
2018-06-04 13:45:37 +0500

    Added Transaction cost view in Write tab

--- src/components/write.js
+++ src/components/write.js
@@ -200,7 +200,7 @@ class Write extends Component {
     }
 
     openConfirmationDialog() {
-        var retVal = confirm("Transaction cost will be " + Math.round(((gasPrice * this.state.gasLimit) / factor + feeToCharge) * Config.ETHToUSDExchangeRate) + " USD " + "Do you want to continue ?");
+        var retVal = confirm("Transaction cost will be $" + Math.round(((gasPrice * this.state.gasLimit) / factor + feeToCharge) * Config.ETHToUSDExchangeRate) + ". Do you want to continue ?");
         if (retVal == true) {
             if (data.ipfsHash != 0) {
                 this.uploadFile()
@@ -272,12 +272,9 @@ class Write extends Component {
             <div>
                 <form onSubmit={this.onSaveData.bind(this)}>
                     <Row style={{ marginBottom: 0 }}>
-
                         <Col s={3}></Col>
                         <Col s={6}>
-
                             <Label>{this.state.currentStatus}</Label>
-
                             <br />
                             <br />
                             <Label style={{ color: 'blue' }}>Save any information on the blockchain fully encrypted. The cost is $1 for data and $5 for a document. Please remember your private key as this will be used to decrypt and read your information when you need it. Use BlockSave to save contracts and other important information that need to be public, but secure and encrypted. BlockSave is useful for Legal, Real Estate, Insurance, Financial contracts and for many other industries.</Label>
@@ -294,13 +291,12 @@ class Write extends Component {
                                 type="file"
                                 onChange={this.captureFile}
                             />
+                            <Label>Transaction cost: ${Math.round(((gasPrice * this.state.gasLimit) / factor + feeToCharge) * Config.ETHToUSDExchangeRate)}</Label>
                             <br />
                             <br />
                             <Label style={{ color: 'blue' }}>Please enter a password here that will be ued to encrypt your data and file. Do not forget this password as you will need it to read your data or file later</Label>
-
                             <Input s={12} type="password" onChange={this.onPrivateKeyChange.bind(this)} name='privateKey' label="Enter Private Key here (used to encrypt data)" />
                             <Row>
-
                                 <Col s={4}></Col>
                                 <Col s={4}>
                                     <Button className="btn waves-effect waves-light" type="submit" name="action" title='submit' style={{ display: 'block', margin: 0, backgroundColor: '#004EFF' }}>Save Data</Button>
